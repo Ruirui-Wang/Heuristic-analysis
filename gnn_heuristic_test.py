615c2d517e84e4977e02a65c673754bb837428b0
ruiruiwang <wangruirui45@outlook.com>
2024-10-23 19:01:45 +0200

    rename

--- gnn_heuristic_test.py
+++ gnn_heuristic_test.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn.functional as F
 import time
 
+from matplotlib import pyplot as plt
 from yacs.config import CfgNode
 
 from ogbdataset import loaddataset
@@ -96,8 +97,11 @@ def train(model, optimizer, data, splits, device, epoch):
 
     # Optimizer step
     optimizer.step()
+    visualize(pos_pred, pos_edge_label, save_path='./visualization_pos_train.png')
+    visualize(neg_pred, neg_edge_label, save_path='./visualization_neg_train.png')
 
     return loss.item()
+
 
 
 @torch.no_grad()
@@ -146,6 +150,8 @@ def test(model, data, splits, device):
     # Predict scores for both positive and negative edges
     pos_pred = model.decode(z[pos_edge_index[0]], z[pos_edge_index[1]])
     neg_pred = model.decode(z[neg_edge_index[0]], z[neg_edge_index[1]])
+    visualize(pos_pred, pos_edge_label, save_path = './visualization_pos.png')
+    visualize(neg_pred, neg_edge_label, save_path = './visualization_neg.png')
 
     # Compute regression loss (MSE)
     pos_loss = F.mse_loss(pos_pred, pos_edge_label)
@@ -163,14 +169,31 @@ def save_to_csv(file_path, model_name, heuristic, test_loss):
         if not file_exists:
             writer.writerow(['Model', 'Heuristic', 'Test_Loss'])
         writer.writerow([model_name, heuristic, test_loss])
+def visualize(pred, true_label, save_path = './visualization.png'):
 
+    pred = pred.cpu().detach().numpy()
+    true_label = true_label.cpu().detach().numpy()
+    plt.figure(figsize=(10, 6))
+    plt.scatter(np.arange(len(true_label)), true_label, color='blue', label='True Score', alpha=0.6)
+    plt.scatter(np.arange(len(pred)), pred, color='red', label='Prediction', alpha=0.6)
+
+    plt.title('Predictions vs True Score')
+    plt.xlabel('Sample Index')
+    plt.ylabel('Value')
+    plt.ylim(0, 1.5)
+    plt.legend()
+
+    plt.savefig(save_path)
+    plt.close()
+
+    print(f"Visualization saved at {save_path}")
 
 
 def parseargs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=1000, help="number of epochs")
-    parser.add_argument('--dataset', type=str, default="ddi")
-    parser.add_argument('--batch_size', type=int, default=128, help="batch size")
+    parser.add_argument('--dataset', type=str, default="Cora")
+    parser.add_argument('--batch_size', type=int, default=512, help="batch size")
     parser.add_argument('--heuristic', type=str, default="CN")
     parser.add_argument('--gnn', type=str, default="gcn")
     parser.add_argument('--model', type=str, default="GIN_Variant")
@@ -190,7 +213,7 @@ if __name__ == "__main__":
     if not hasattr(splits['train'], 'x') or splits['train'].x is None:
         cfg_model.in_channels = 1024
     else:
-        cfg_model.in_channels = splits['train'].x.shape[1]
+        cfg_model.in_channels = data.num_nodes
     cfg_score = eval(f'cfg.score.{args.model}')
     cfg.model.type = args.model
     edge_weight = torch.ones(data.edge_index.size(1), dtype=float)
@@ -204,17 +227,20 @@ if __name__ == "__main__":
         "RA": RA
     }
     for split in splits:
-        splits[split]['pos_edge_score'], _ = method_dict[args.heuristic](A, splits[split]['pos_edge_label_index'], batch_size=args.batch_size)
-        splits[split]['neg_edge_score'], _ = method_dict[args.heuristic](A, splits[split]['neg_edge_label_index'], batch_size=args.batch_size)
-
+        pos_edge_score, _ = method_dict[args.heuristic](A, splits[split]['pos_edge_label_index'],
+                                                        batch_size=args.batch_size)
+        neg_edge_score, _ = method_dict[args.heuristic](A, splits[split]['neg_edge_label_index'],
+                                                        batch_size=args.batch_size)
+        splits[split]['pos_edge_score'] = torch.sigmoid(pos_edge_score)
+        splits[split]['neg_edge_score'] = torch.sigmoid(neg_edge_score)
     if not args.use_feature:
-        data.x = torch.randn((data.num_nodes, cfg_model.in_channels)).to(device)
+        data.x = torch.eye(data.num_nodes, data.num_nodes).to(device)
 
     model = create_GAE_model(cfg_model, cfg_score, args.model).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     for epoch in range(1, args.epochs + 1):
         start = time.time()
-        loss = train(model, optimizer, data, splits, device,epoch)
+        loss = train(model, optimizer, data, splits, device, args.batch_size)
         print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
     test_loss = test(model, data, splits, device)
     save_to_csv(f'./results/test_results_{args.dataset}.csv', args.model, args.heuristic, test_loss)
